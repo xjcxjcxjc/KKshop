af33e325b175e0c43c4313ea8cbf29a8457a452a
xjc <2521220414@qq.com>
2020-11-10 10:12:50 +0800

    First commit.

---
+++ app/src/main/java/com/example/kkshop/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.kkshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kkshop.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
