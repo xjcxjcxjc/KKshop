405fe4a55a8dd7687e0dac95f4c093f18f5193b8
xjc <2521220414@qq.com>
2020-11-23 20:32:15 +0800

    MainPage

--- app/src/main/java/com/example/kkshop/MainActivity.java
+++ app/src/main/java/com/example/kkshop/MainActivity.java
@@ -1,15 +1,43 @@
 package com.example.kkshop;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
-import com.example.kkshop.R;
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
 
-public class MainActivity extends AppCompatActivity {
+    RadioGroup rg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
     }
+
+    private void initView(){
+        rg=findViewById(R.id.rg);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId){
+            case R.id.rb_1:
+
+                break;
+            case R.id.rb_2:
+
+                break;
+            case R.id.rb_3:
+
+                break;
+            case R.id.rb_4:
+
+                break;
+        }
+    }
+
 }
+
